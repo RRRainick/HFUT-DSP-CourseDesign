dd162519b5997ff719082ff28eaf50ac841f298a
Rainick <nick.yangtze@outlook.com>
2023-02-04 13:12:26 +0800

    v1.1 * optimize branch structure and sequence writting process * adapate matlab output format

--- main.c
+++ main.c
@@ -7,11 +7,11 @@
 int main(int argc, char *argv[])
 {
     float input_sequence[LENGTH][WIDTH]; // 默认零矩阵，满足序列自动补零
+    float *ptr_input = input_sequence; 
     unsigned char tmp_array[32]; // float类型在x64上占64bit
     char ch;
     int digit = 0;
     double index_decp = 0.0;
-    int i_input = 0; int j_input = 0;
     _Bool plus_sign = 1;
     double tmp_num = 0.0;
     FILE *fp;
@@ -28,42 +28,38 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     while((ch = getc(fp)) != EOF){
-        // sign check
-        if(ch == '-'){
-            plus_sign = 0;
-            continue;
-        }
-        else if(ch == '.'){
-            index_decp = digit;
-            continue;
-        }
-        else if(ch == ' ' || ch == '\n'){
-            if(index_decp == 0){
-                for(int i = 0; i < digit; i++){
-                    tmp_num += tmp_array[i] * pow(10.0, digit-1.0-i);
+        switch(ch){
+            case '.':
+                index_decp = digit;
+                break;
+            case '-':
+                plus_sign = 0;
+                break;
+            case ' ':
+            case '\n':
+                if(index_decp == 0){
+                    for(int i = 0; i < digit; i++){
+                            tmp_num += tmp_array[i] * pow(10.0, digit-1.0-i);
+                        }
                 }
-            }
-            else{
-                for(int i = 0; i < digit; i++){
-                    tmp_num += tmp_array[i] * pow(10.0, index_decp-1.0-i);
+                else{
+                    for(int i = 0; i < digit; i++){
+                        tmp_num += tmp_array[i] * pow(10.0, index_decp-1.0-i);
+                    }
                 }
-            }
-            if(!plus_sign)
-                tmp_num = (-1.0)*tmp_num;
-            if(j_input == 2){
-                i_input++;
-                j_input = 0;
-            }
-            input_sequence[i_input][j_input] = tmp_num;
-            printf("%f\n", tmp_num);
-            digit = 0; tmp_num = 0.0; plus_sign = 1; index_decp = 0.0; j_input++; // variable reset
-        }
-        else{
-           tmp_array[digit++] = ch - '0'; // char2short
+                if(!plus_sign)
+                    tmp_num = (-1.0)*tmp_num;
+                *ptr_input = tmp_num;
+                ptr_input++;
+                printf("%f\n", tmp_num);
+                digit = 0; tmp_num = 0.0; plus_sign = 1; index_decp = 0.0;  // variable reset
+                break;
+            default:
+                tmp_array[digit++] = ch - '0'; // char2short
+                break;
         }
     }
     fclose(fp);
-
 
     return 0;
 }
