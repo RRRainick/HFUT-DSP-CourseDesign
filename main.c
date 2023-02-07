1fd15ddb59c19422ff61e9c3ea3b1b5c11407c9a
Rainick <nick.yangtze@outlook.com>
2023-02-07 16:55:33 +0800

    v2.1 * encapsulate resort() function * ms timer

--- main.c
+++ main.c
@@ -1,15 +1,46 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<complex.h>
-#include"br_lookup_table.h"
-#include"WN_lookup_table.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+#include <windows.h>
+#include "br_lookup_table.h"
+#include "WN_lookup_table.h"
 
 #ifdef LENGTH
 #else
     #define LENGTH 2048
     #define WIDTH 2
 #endif
+
+LARGE_INTEGER freq;
+LARGE_INTEGER start, end;
+double resort_ms, reverse_ms, btf_ms;
+
+_Bool read_file(float *ptr_seq, int argc, char *argv[]);
+float complex *resort_seq(_Bool complex_sign, float const const (*str_input)[WIDTH], void *xn, float complex *real_xn, float complex *complex_xn);
+void reverse_seq(_Bool complex_sign, float complex *xn);
+void compute_butterfly(_Bool complex_sign, float complex *const r_xn);
+void write_file(_Bool complex_sign, float complex *Xk);
+
+int main(int argc, char *argv[])
+{
+    float input_seq[LENGTH][WIDTH]; // 默认零矩阵，满足序列自动补零
+    _Bool complex_sign = read_file(input_seq, argc, argv);
+    float const (*str_input)[WIDTH] = input_seq;
+    void *xn;   // 输入序列指针
+    float complex real_xn[LENGTH*WIDTH]; float complex complex_xn[LENGTH]; // 由于C语言不支持函数重载，因此将real_xn和complex_xn均定义为float complex类型
+    xn = resort_seq(complex_sign, str_input, xn, real_xn, complex_xn);
+   
+    reverse_seq(complex_sign, xn);
+
+    compute_butterfly(complex_sign, xn);
+    
+    write_file(complex_sign, xn);
+
+    printf("resort cost %fms, reverse cost %fms, compute butterfly cost %fms.\nduration is %fms.\n", resort_ms, reverse_ms, btf_ms, resort_ms+reverse_ms+btf_ms);
+    return 0;
+}
+
 
 _Bool read_file(float *ptr_seq, int argc, char *argv[])
 {
@@ -91,9 +122,35 @@ _Bool read_file(float *ptr_seq, int argc, char *argv[])
 
 }
 
+float complex *resort_seq(_Bool complex_sign, float const (*str_input)[WIDTH], void *xn, float complex *real_xn, float complex *complex_xn)
+{
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&start);
+    float complex *output_xn;
+    if(complex_sign){
+        for(int i = 0; i < LENGTH; i++){
+            complex_xn[i] = (*str_input)[0] + (*str_input)[1] * I;
+            str_input++;
+        }
+        output_xn = complex_xn;
+    }
+    else{
+        for(int j = 0; j < LENGTH * WIDTH; j = j + 2){
+           real_xn[j] = (*str_input)[0];
+           real_xn[j+1] = (*str_input)[1];
+           str_input++; 
+        }
+        output_xn = real_xn;
+    }
+    QueryPerformanceCounter(&end);
+    resort_ms = (double)(end.QuadPart - start.QuadPart) * 1e6 / (double)(freq.QuadPart);
+    return output_xn;
+} 
 
 void reverse_seq(_Bool complex_sign, float complex *xn)
 {
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&start);
     // log_{2}^{2048}=11, log_{2}^{4096}=12
     unsigned int count;
     unsigned int const *ptr_br_table;
@@ -116,10 +173,15 @@ void reverse_seq(_Bool complex_sign, float complex *xn)
             i_arr_sign[k] = 1; i_arr_sign[index] = 1;
         }
     }
+    QueryPerformanceCounter(&end);
+    reverse_ms = (double)(end.QuadPart - start.QuadPart) * 1e6 / (double)(freq.QuadPart);
+    
 }
 
-void fft(_Bool complex_sign, float complex * const r_xn)
+void compute_butterfly(_Bool complex_sign, float complex * const r_xn)
 {
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&start);
     unsigned int length;
     float complex const *r_ptr_WN_table;
     float complex const *o_ptr_WN_table;
@@ -171,11 +233,13 @@ void fft(_Bool complex_sign, float complex * const r_xn)
         j_max /= 2;
         k_max *= 2;
     }
+    QueryPerformanceCounter(&end);
+    btf_ms = (double)(end.QuadPart - start.QuadPart) * 1e6 / (double)(freq.QuadPart);
 }
 
-void write_file(_Bool complex_sign, float complex *str_Xk)
+void write_file(_Bool complex_sign, float complex *Xk)
 {
-    float complex const *str_output = str_Xk;
+    float complex const *str_output = Xk;
     unsigned int count;
     FILE *fp;
     if(complex_sign)
@@ -198,38 +262,6 @@ void write_file(_Bool complex_sign, float complex *str_Xk)
         count--;
     }
     fclose(fp);
-}
-
-int main(int argc, char *argv[])
-{
-    float input_seq[LENGTH][WIDTH]; // 默认零矩阵，满足序列自动补零
-    _Bool complex_sign = read_file(input_seq, argc, argv);
-    float const (*str_input)[WIDTH] = input_seq;
-    void *xn;   // 输入序列指针
-    float complex real_xn[LENGTH*WIDTH]; float complex complex_xn[LENGTH]; // 由于C语言不支持函数重载，因此将real_xn和complex_xn均定义为float complex类型
-
-    if(complex_sign){
-        for(int i = 0; i < LENGTH; i++){
-            complex_xn[i] = (*str_input)[0] + (*str_input)[1] * I;
-            str_input++;
-        }
-        xn = complex_xn;
-    }
-    else{
-        for(int j = 0; j < LENGTH * WIDTH; j = j + 2){
-           real_xn[j] = (*str_input)[0];
-           real_xn[j+1] = (*str_input)[1];
-           str_input++; 
-        }
-        xn = real_xn;
-    }
-    
-    reverse_seq(complex_sign, xn);
-
-    fft(complex_sign, xn);
-    
-    write_file(complex_sign, xn);
-    return 0;
 }
 
    
