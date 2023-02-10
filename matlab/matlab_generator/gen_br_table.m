e3de8bf86e1ac1736bd133e6addd4b330b33675b
Rainick <nick.yangtze@outlook.com>
2023-02-10 23:46:04 +0800

    * add comment

--- matlab/matlab_generator/gen_br_table.m
+++ matlab/matlab_generator/gen_br_table.m
@@ -4,7 +4,7 @@ N = 2048;
 n = (0: N-1);
 x = bitrevorder(n); %% 生成bitreverse序列
 xm = reshape(x, [16, 128]); %% reshape矩阵使其长度限制为16，方便在编辑器中查看
-file_dir = fopen('../../data/complex_br_table.txt', 'w'); %% 打开输出文件
+file_dir = fopen('../../data/br_table.txt', 'w'); %% 打开输出文件
 for i = (1: 128)
     fprintf(file_dir, '%d,\t', xm(:, i));
     fprintf(file_dir, '\n');
@@ -12,6 +12,7 @@ end
 fclose(file_dir);
 
 %% 生成实序列（长度4096）的对应bitreverse序列查找表
+%{
 clear;
 N = 4096;
 n = (0: N-1);
@@ -23,3 +24,4 @@ for i = (1: 256)
     fprintf(file_dir, '\n');
 end
 fclose(file_dir);
+%}
