f5472782235a5452a18abed1f7ec9925b138ddee
Rainick <nick.yangtze@outlook.com>
2023-02-12 16:41:32 +0800

    * modified plotter

--- matlab/matlab_plotter/FFT_figure.m
+++ matlab/matlab_plotter/FFT_figure.m
@@ -33,7 +33,7 @@ Nxc = length(xnc);
 nc = (0: Nxc-1);
 xnc_x_lim = [0, Nxc-1];
 
-fs = 1e6;
+fs = 1e3;
 res = fs/Nx; %% omg_res = 2*pi*fs/Nx;
 resc = fs/Nkc; %% omg_resc = 2*pi*fs/Nkc;
 xt_x_lim = xn_x_lim ./ fs;  Xomg_x_lim = [0, fs/2]; Xomg_py_lim = [-pi, pi];
@@ -45,46 +45,46 @@ if ~isreal(xn)
     xn_arange = xn_amax - xn_amin; xn_ay_lim = [xn_amin-xn_arange*0.1, xn_amax+xn_arange*0.1]; %% 计算幅度最值，用于确定绘图坐标轴范围
     %% 若原信号为复信号，则图像用幅时图像表示
     %% 注意y轴范围为xn_amin - xn_amin * 0.1 
-    subplot(3, 3, [1, 2, 3]); stem(n, abs(xn_f)); axis([xn_x_lim, xn_ay_lim]); title('原信号x[n]时域图像');
-    subplot(3, 3, 6); stem(n, abs(xn_k)); axis([xn_x_lim, xn_ay_lim]); title('matlab IFFT恢复信号'); 
+    subplot(3, 3, [1, 2, 3]); stem(n, abs(xn_f)); axis([xn_x_lim, xn_ay_lim]); xlabel('n'); ylabel('|x[n]|'); title('原信号x[n]时域图像');
+    subplot(3, 3, 6); stem(n, abs(xn_k)); axis([xn_x_lim, xn_ay_lim]); xlabel('n'); ylabel('|x[n]|'); title('matlab IFFT恢复信号'); 
 else
     %% 若原信号为是信号，则无需计算幅度
     %% 注意y轴范围为xn_amin + xn_amin * 0.1
     xn_amax = max(xn); xn_amin = min(xn); 
     xn_arange = xn_amax - xn_amin; xn_ay_lim = [xn_amin-xn_arange*0.1, xn_amax+xn_arange*0.1]; %% 计算幅度最值，用于确定绘图坐标轴范围
-    subplot(3, 3, [1, 2, 3]); stem(n, xn_f); axis([xn_x_lim, xn_ay_lim]); title('原信号x[n]时域图像');
-    subplot(3, 3, 6); stem(n, xn_k); axis([xn_x_lim, xn_ay_lim]); title('matlab IFFT恢复信号'); 
+    subplot(3, 3, [1, 2, 3]); stem(n, xn_f); axis([xn_x_lim, xn_ay_lim]); xlabel('n'); ylabel('x[n]'); title('原信号x[n]时域图像');
+    subplot(3, 3, 6); stem(n, xn_k); axis([xn_x_lim, xn_ay_lim]); xlabel('n'); ylabel('x[n]'); title('matlab IFFT恢复信号'); 
 end
 if ~isreal(xnc)
     xnc_amax = max(abs(xnc)); xnc_amin = min(abs(xnc));
     xnc_arange = xnc_amax - xnc_amin; xnc_ay_lim = [xnc_amin-xnc_arange*0.1, xnc_amax+xnc_arange*0.1]; %% 计算幅度最值，用于确定绘图坐标轴范围
-    subplot(3, 3, 9); stem(nc, abs(xnc)); axis([xnc_x_lim, xnc_ay_lim]); title('C IFFT恢复信号'); 
+    subplot(3, 3, 9); stem(nc, abs(xnc)); axis([xnc_x_lim, xnc_ay_lim]); xlabel('n'); ylabel('|x[n]|'); title('C IFFT恢复信号'); 
 else
     xnc_amax = max(xnc); xnc_amin = min(xnc);
     xnc_arange = xnc_amax - xnc_amin; xnc_ay_lim = [xnc_amin-xnc_arange*0.1, xnc_amax+xnc_arange*0.1]; %% 计算幅度最值，用于确定绘图坐标轴范围
-    subplot(3, 3, 9); stem(nc, xnc); axis([xnc_x_lim, xnc_ay_lim]); title('C IFFT恢复信号'); 
+    subplot(3, 3, 9); stem(nc, xnc); axis([xnc_x_lim, xnc_ay_lim]); xlabel('n'); ylabel('x[n]'); title('C IFFT恢复信号'); 
 end
-    subplot(3, 3, 4); stem(k, abs(Xk));  axis([Xk_x_lim, Xk_ay_lim]); title('matlab FFT幅频特性');
-    subplot(3, 3, 5); stem(k, angle(Xk));  axis([Xk_x_lim, Xk_py_lim]); title('matlab FFT相频特性');
-    subplot(3, 3, 7); stem(kc, abs(Xkc_f));  axis([Xkc_x_lim, Xkc_ay_lim]); title('C FFT幅频特性');
-    subplot(3, 3, 8); stem(kc, angle(Xkc_f));  axis([Xkc_x_lim, Xkc_py_lim]); title('C FFT相频特性');
+    subplot(3, 3, 4); stem(k, abs(Xk));  axis([Xk_x_lim, Xk_ay_lim]); xlabel('k'); ylabel('|X[k]|'); title('matlab FFT幅频特性');
+    subplot(3, 3, 5); stem(k, angle(Xk));  axis([Xk_x_lim, Xk_py_lim]); xlabel('k'); ylabel('\Phi'); title('matlab FFT相频特性');
+    subplot(3, 3, 7); stem(kc, abs(Xkc_f));  axis([Xkc_x_lim, Xkc_ay_lim]); xlabel('k'); ylabel('|X[k]|'); title('C FFT幅频特性');
+    subplot(3, 3, 8); stem(kc, angle(Xkc_f));  axis([Xkc_x_lim, Xkc_py_lim]); xlabel('k'); ylabel('\Phi'); title('C FFT相频特性');
 
 figure2 = figure('name', 'FFT的频域分析');
 if ~isreal(xn)
-    subplot(3, 3, [1, 2, 3]); plot(n/fs, abs(xn_f)); axis([xt_x_lim, xn_ay_lim]); title('原信号x[n]时域图像');
-    subplot(3, 3, 6); plot(n/fs, abs(xn_k)); axis([xt_x_lim, xn_ay_lim]); title('matlab IFFT恢复信号'); 
+    subplot(3, 3, [1, 2, 3]); plot(n/fs, abs(xn_f)); axis([xt_x_lim, xn_ay_lim]); xlabel('t(s)'); ylabel('|x(t)|'); title('原信号x[n]时域图像');
+    subplot(3, 3, 6); plot(n/fs, abs(xn_k)); axis([xt_x_lim, xn_ay_lim]); xlabel('t(s)'); ylabel('|x(t)|'); title('matlab IFFT恢复信号'); 
 else
-    subplot(3, 3, [1, 2, 3]); plot(n/fs, xn_f); axis([xt_x_lim, xn_ay_lim]); title('原信号x[n]时域图像');
-    subplot(3, 3, 6); plot(n/fs, xn_k); axis([xt_x_lim, xn_ay_lim]); title('matlab IFFT恢复信号'); 
+    subplot(3, 3, [1, 2, 3]); plot(n/fs, xn_f); axis([xt_x_lim, xn_ay_lim]); xlabel('t(s)'); ylabel('x(t)'); title('原信号x[n]时域图像');
+    subplot(3, 3, 6); plot(n/fs, xn_k); axis([xt_x_lim, xn_ay_lim]); xlabel('t(s)'); ylabel('x(t)'); title('matlab IFFT恢复信号'); 
 end
 if ~isreal(xnc)
-    subplot(3, 3, 9); plot(nc/fs, abs(xnc)); axis([xtc_x_lim, xnc_ay_lim]); title('C IFFT恢复信号'); 
+    subplot(3, 3, 9); plot(nc/fs, abs(xnc)); axis([xtc_x_lim, xnc_ay_lim]); xlabel('t(s)'); ylabel('|x(t)|'); title('C IFFT恢复信号'); 
 else
-    subplot(3, 3, 9); plot(nc/fs, xnc); axis([xtc_x_lim, xnc_ay_lim]); title('C IFFT恢复信号'); 
+    subplot(3, 3, 9); plot(nc/fs, xnc); axis([xtc_x_lim, xnc_ay_lim]); xlabel('t(s)'); ylabel('x(t)'); title('C IFFT恢复信号'); 
 end
-    subplot(3, 3, 4); plot(k*res, abs(Xk));  axis([Xomg_x_lim, Xk_ay_lim]); title('matlab FFT幅频特性');
-    subplot(3, 3, 5); plot(k*res, angle(Xk));  axis([Xomg_x_lim, Xomg_py_lim]); title('matlab FFT相频特性');
-    subplot(3, 3, 7); plot(kc*resc, abs(Xkc_f));  axis([Xomgc_x_lim, Xkc_ay_lim]); title('C FFT幅频特性');
-    subplot(3, 3, 8); plot(kc*resc, angle(Xkc_f));  axis([Xomgc_x_lim, Xomgc_py_lim]); title('C FFT相频特性');
+    subplot(3, 3, 4); plot(k*res, abs(Xk));  axis([Xomg_x_lim, Xk_ay_lim]); xlabel('f(Hz)'); ylabel('|X(f)|'); title('matlab FFT幅频特性');
+    subplot(3, 3, 5); plot(k*res, angle(Xk));  axis([Xomg_x_lim, Xomg_py_lim]); xlabel('f(Hz)'); ylabel('\Phi(rad)'); title('matlab FFT相频特性');
+    subplot(3, 3, 7); plot(kc*resc, abs(Xkc_f));  axis([Xomgc_x_lim, Xkc_ay_lim]); xlabel('f(Hz)'); ylabel('|X(f)|'); title('C FFT幅频特性');
+    subplot(3, 3, 8); plot(kc*resc, angle(Xkc_f));  axis([Xomgc_x_lim, Xomgc_py_lim]); xlabel('f(Hz)'); ylabel('\Phi(rad)'); title('C FFT相频特性');
 
 fprintf("duration is %fus\n", duration);
